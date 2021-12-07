e7e8a953a85624bab4e30915e5a29de2cd193f1f
Lucas A. Ouverney <realfakenerd@users.noreply.github.com>
2021-12-07 20:14:20 -0300

    removed things

---
+++ src/components/Nav.svelte
@@ -0,0 +1,14 @@
+<div class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
+  <ul>
+    <li>
+      <a href="/">
+        <span>Home</span>
+      </a>
+    </li>
+    <li>
+      <a href="/about">
+        <span>About</span>
+      </a>
+    </li>
+  </ul>
+</div>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -6,11 +6,3 @@
 </script>
 
 <h1>Routify Starter Template</h1>
-
-<p>
-    To see an example app that shows off a lot of Routify's features, go to <a href="/example">/example</a>
-</p>
-
-<p>
-    This template is ready to be used in production! just delete the example app at: src/pages/example
-</p>
